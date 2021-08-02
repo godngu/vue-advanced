d71b6b9e6fe8449fb5db65890b79b96ad21f4522
kil <godngu@gmail.com>
2021-08-02 16:05:15 +0900

    router, toolbar 적용

---
+++ vue-news/src/routes/index.js
@@ -0,0 +1,42 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import NewsView from "@/views/NewsView";
+import AskView from "@/views/AskView";
+import JobsView from "@/views/JobsView";
+import UserView from "@/views/UserView";
+import ItemView from "@/views/ItemView";
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/news'
+    },
+    {
+        path: '/news',
+        name: 'News',
+        component: NewsView
+    },
+    {
+        path: '/ask',
+        name: 'Ask',
+        component: AskView
+    },
+    {
+        path: '/jobs',
+        name: 'Jobs',
+        component: JobsView
+    },
+    {
+        path: '/item',
+        name: 'Item',
+        component: ItemView
+    },
+    {
+        path: '/user',
+        name: 'User',
+        component: UserView
+    }
+]
+export const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
---
+++ vue-news/src/views/ItemView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Item</div>
+</template>
+
+<script>
+export default {
+    name: "ItemView"
+}
+</script>
+
+<style scoped>
+
+</style>
