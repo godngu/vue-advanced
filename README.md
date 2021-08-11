d5dd1315eb8e64e42e0fee92297c43983542c190
kil <godngu@gmail.com>
2021-08-11 11:34:45 +0900

    common, slots component 예제

--- README.md
+++ README.md
@@ -16,9 +16,11 @@ $ npm i vue-router@next
 $ npm i vuex@next --save
 ```
 
-
-
-
-
 ## async-await 프로젝트 생성
 $ vue create async-await
+
+## 컴포넌트 디자인 패턴
+1. Common : 기본적인 컴포넌트 등록과 컴포넌트 통신
+2. Slot : 마크업 확장이 가능한 컴포넌트
+3. Controlled : 결합력이 높은 컴포넌트
+4. Renderless : 데이터 처리 컴포넌트
---
+++ design/common/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <app-header :title="title"></app-header>
+    <app-content :items="items" @renew="renewItems"></app-content>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+import AppContent from "@/components/AppContent";
+
+export default {
+    name: 'App',
+    components: {
+        AppContent,
+        AppHeader
+    },
+    data() {
+        return {
+            title: 'Common Approach',
+            items: [10, 20, 30],
+        }
+    },
+    methods: {
+        renewItems() {
+            this.items = [40, 50, 60]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
